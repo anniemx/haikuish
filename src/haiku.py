8ece5079140a36d6e6f20471abf6fdaa25189ccb
anniemx <163184360+anniemx@users.noreply.github.com>
2025-11-01 17:59:50 +0200

    Add basic structure to project

---
+++ src/haiku.py
@@ -0,0 +1,11 @@
+class Haiku():
+    def __init__(self):
+        pass
+
+    def generate_haiku(self):
+        #tavut kolmella rivillä 5 - 7 - 5
+        pass
+
+    def print_haiku(self):
+        #tulosta rivi kerrallaan
+        pass
