b646406dc0e37c18a71eb5c75e711eae799c72e6
anniemx <163184360+anniemx@users.noreply.github.com>
2025-11-03 21:05:32 +0200

    Add transition matrix

--- src/process_data.py
+++ src/process_data.py
@@ -17,10 +17,12 @@ corpus = re.sub(r"[^a-zA-Z0-9\s]", "", corpus)
 corpus = corpus.lower()
 
 def process():
-    generate_ngrams(corpus)
-    find_unique_words(corpus)
+    all_n_grams = generate_ngrams(corpus)
+    all_words = find_unique_words(corpus)
+    trasition_matrix = create_transition_matrix(len(all_words))
 
-#jaetaan tekstiaineisto 3 sanan ketjuihin (n-grammeihin, n=3)
+
+#jaetaan tekstiaineisto kolmen sanan ketjuihin (n-grammeihin, n=3)
 def generate_ngrams(corpus):
     n = 3
     words = corpus.split()
@@ -30,7 +32,6 @@ def generate_ngrams(corpus):
     print(n_grams)
     return n_grams
 
-
     #n_grams = ngrams(text.split(), n)
     #n_grams = list(n_grams)
 
@@ -39,8 +40,10 @@ def find_unique_words(corpus):
     print(word_list, len(word_list))
     return word_list
 
-def transition_matrix():
-    pass
+def create_transition_matrix(n_words):
+    transition_matrix = [[0] * n_words for i in range(n_words)]
+    print(len(transition_matrix[1]))
+    return transition_matrix
 
 def calculate_probabilities():
     pass
