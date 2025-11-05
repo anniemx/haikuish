c68509ebc611d12c4b40f11d2634778b52f1eb8c
anniemx <163184360+anniemx@users.noreply.github.com>
2025-11-05 20:08:26 +0200

    Add trie-structure

---
+++ src/trie.py
@@ -0,0 +1,15 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None]
+        self.end = False
+
+
+class Trie:
+    def trie_find():
+        pass
+
+    def trie_insert(root, key):
+        pass
+
+    def trie_delete():
+        pass
