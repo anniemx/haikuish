683cbe5a0a953e1e296148c5c26cb3f58d7e15e6
anniemx <163184360+anniemx@users.noreply.github.com>
2025-11-10 20:05:14 +0200

    Plan tests

--- src/tests/test_markov.py
+++ src/tests/test_markov.py
@@ -7,11 +7,22 @@ class TestMarkovModel(unittest.TestCase):
     def setUp(self):
         self.markov = MarkovModel()
 
-    def test_demo(self):
-        self.assertEqual("", "") #assertEqual-testi
 
+    """testataan, että malli rakentuu oikein: n-grammit jakautuu mallin sanakirjarakenteeseen muotoon 
+    (sana1, sana2):[(seuraava_sana1, x), (seuraava_sana2, x), (seuraava_sana, x)]"""
+    def test_build_model_(self):
+        self.assertEqual()
+
+    """testataan, että todennäköisyys oikein"""
+    def calculate_prob(self):
+        prob =  self.markov
+        self.assertEqual()
+
+    """testataan, ettei tyhjiä mjonoja"""
     def test_empty_string(self):
         self.assertEqual("", "")
 
+    """testataan, ettei None"""
     def test_none(self):
-        self.assertEqual("", "")
+        self.assertEqual("", None)
+
