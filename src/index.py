4c75fe9a2efd1fd0c0570c71719f36bbed61f7d3
anniemx <163184360+anniemx@users.noreply.github.com>
2025-12-14 13:32:00 +0200

    Add def main

--- src/index.py
+++ src/index.py
@@ -3,20 +3,23 @@ import markov
 import trie
 import haiku
 
+def main():
+    #ladataan ja käsitellään tekstiaineisto
+    corpus = process_data.process(path = "src/data/corpus_syllables.txt")
+    haiku_poem = haiku.Haiku()
+    k_order = haiku_poem.k_order() #haetaan käyttäjältä markovin ketjun haluttu aste
+    #luodaan markovin ketju halutun asteen mukaan
+    markov_model = markov.MarkovModel(k_order, corpus)
+    markov_chain = markov_model.build_model()
+    trie_tree = trie.Trie()
+    trie_tree.trie_insert_markov_chain(markov_chain) #tallennetaan sekvenssit ja frekvenssit trieen
 
-#ladataan ja käsitellään tekstiaineisto
-corpus = process_data.process(path = "src/data/corpus_syllables.txt")
-haiku = haiku.Haiku()
-k_order = haiku.k_order() #haetaan käyttäjältä markovin ketjun haluttu aste
-#luodaan markovin ketju halutun asteen mukaan
-markov_model = markov.MarkovModel(k_order, corpus)
-markov_chain = markov_model.build_model()
-trie = trie.Trie()
-trie.trie_insert_markov_chain(markov_chain) #tallennetaan sekvenssit ja frekvenssit trieen
+    #generoidaan sanat ja tarkastetaan haikumuoto
+    try:
+        content = trie_tree.generate(k_order, length=500)
+        haiku_poem.print_haiku(content)
+    except:
+        print("Haikumuotoa noudattavia runoja ei löytynyt.")
 
-#generoidaan sanat ja tarkastetaan haikumuoto
-try:
-    content = trie.generate(k_order, length=500)
-    haiku.print_haiku(content)
-except:
-    print("Haikumuotoa noudattavia runoja ei löytynyt.")
+if __name__=="__main__":
+    main()
