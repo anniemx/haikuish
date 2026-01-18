0471802afa490b9b7d95a31b592323381f603c66
anniemx <163184360+anniemx@users.noreply.github.com>
2026-01-18 14:43:16 +0200

    Add trie tests

--- src/tests/test_trie.py
+++ src/tests/test_trie.py
@@ -1,35 +1,37 @@
 import unittest
-from ..trie import Trie
+from src.trie import Trie
 
 class TestTrie(unittest.TestCase):
     def setUp(self):
         self.trie = Trie()
-        self.content = {(("testaus", 2), ("teksti", 2), ("jossa", 2)): 1, 
-                        (("teksti", 2), ("jossa", 2), ("on", 2)): 1,
-                        (("jossa", 2), ("on", 2), ("useampi", 3)): 1, 
-                        (("on", 2), ("useampi", 3), ("lapsi", 2)) : 1,
-                        (("useampi", 3), ("lapsi", 2), ("ja", 1)): 1, 
-                        (("lapsi", 2), ("ja", 1), ("useampi", 4)): 1,
-                        (("ja", 1), ("useampi", 4), ("sana", 2)): 3, 
-                        (("useampi", 4), ("sana", 2), ("jotta", 2)): 1,
-                        (("sana", 2), ("jotta", 2), ("nähdään", 2)): 1, 
-                        (("jotta", 2), ("nähdään", 2), ("jakaako", 3)): 1,
-                        (("nähdään", 2), ("jakaako", 3), ("trie", 1)): 1, 
-                        (("jakaako", 3), ("trie", 1), ("rakenteeseen", 4)): 1,
-                        (("trie", 1), ("rakenteeseen", 4), ("oikein", 2)): 1}
-        self.trie.trie_insert_markov_chain(self.content)
+        self.trie.trie_insert([('on', '1'), ('virrannut', '3'), ('lastenlaulukulttuurin', '7')])
+        self.trie.trie_insert([('virrannut', '3'), ('lastenlaulukulttuurin', '7'), ('uudistamisesta', '6')])
+        self.trie.trie_insert([('lastenlaulukulttuurin', '7'), ('uudistamisesta', '6'), ('ja', '1')])
+        self.trie.trie_insert([('on', '1'), ('olemassa', '4'), ('monia', '3')])
 
-    def test_trie_insert_markov_chain(self):
-        self.assertTrue(self.trie.trie_search((("ja", 1), ("useampi", 4), ("sana", 2))))
-        self.assertTrue(self.trie.trie_search((("jotta", 2), ("nähdään", 2), ("jakaako", 3))))
-        self.assertTrue(self.trie.trie_search((("trie", 1), ("rakenteeseen", 4), ("oikein", 2))))
+    def test_trie_insert(self):
+        self.assertTrue(self.trie.trie_search([('on', '1'), ('virrannut', '3'), ('lastenlaulukulttuurin', '7')]))
+        self.assertTrue(self.trie.trie_search([('virrannut', '3'), ('lastenlaulukulttuurin', '7'), ('uudistamisesta', '6')]))
+        self.assertTrue(self.trie.trie_search([('lastenlaulukulttuurin', '7'), ('uudistamisesta', '6'), ('ja', '1')]))
+        self.assertTrue(self.trie.trie_search([('on', '1'), ('olemassa', '4'), ('monia', '3')]))
 
-    def test_search_empty_string(self):
+    def test_search_empty_sequence(self):
         self.assertEqual(self.trie.trie_search(""), False)
 
     def test_search_none(self):
-        result = self.trie.generate(None, None)
-        self.assertEqual(result, [])
+        result = self.trie.trie_search(None)
+        self.assertEqual(result, False)
 
-    def test_structure(self):
-        print(self.trie)
+    def test_get_followers(self):
+        #test structure and syllable filter
+        search_words = [('on', '1')]
+        limit1 = 5
+        limit2 = 3
+        result1 = self.trie.trie_get_followers(search_words, limit1)
+        result2 = self.trie.trie_get_followers(search_words, limit2)
+        print(result1)
+        print(result2)
+        self.assertEqual(len(result1[0]), 2) #children should be "virrannut" and "olemassa"
+        self.assertEqual(len(result2[0]), 1) ##children should be "virrannut"
+        
+
