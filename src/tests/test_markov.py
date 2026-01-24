5f44d169b5a9da82309044b1eb46f60a0a381564
anniemx <163184360+anniemx@users.noreply.github.com>
2026-01-24 13:25:20 +0200

    Add markov-chain test

--- src/tests/test_markov.py
+++ src/tests/test_markov.py
@@ -1,13 +1,50 @@
+from src import process_data
+from src import haiku
 
-"""Päästä-päähän testaus
-    1. lataa testitekstiaineisto (pienempi ja kevyempi), muodosta sen n-grammit
-    2. generoi haiku
-    3. muodosta runosta n-grammit
-    4. etsi löytyykö muodostetut n-grammit alkuperäisestä aineistosta
+"""End-to-end testing
+    1. load testing data, generate ngrams k-order
+    2. generate haiku
+    3. generate ngrams from the haiku k-order
+    4. check if the ngrams exist in the original corpus
     """
 
-class TestMarkovModel():
-    pass
+def test_markov_chain():
+    #load and process corpus
+    corpus = process_data.process(path = "src/data/test_corpus.txt")
+    haiku_poem = haiku.Haiku()
+    k_order = 2
+    #insert sentences by k+1 ngrams to trie
+    haiku_poem.create_trie(k_order, corpus)
+
+    try:
+        haiku_poem.generate_haiku(k_order)
+    except:
+        print("Haikumuotoa noudattavia runoja ei voitu generoida.")
+    
+    #generate n-grams from haiku
+    haiku_words = [word for line in haiku_poem for word in line]
+    corpus_ngrams = []
+    n_grams = []
+    n = k_order + 1
+
+    for sentence in corpus: 
+        for i in range(len(sentence) - n + 1):
+            ngram1 = sentence[i: i + n]
+            ngram_words = []
+            for word in ngram1: #[sana1:1 sana2:2 sana3:2]
+                ngram_words.append(word[0])
+            corpus_ngrams.append(ngram_words)
+
+    for i in range(len(haiku_words) - n + 1):
+        n_grams.append(haiku_words[i: i + n])
+
+    result = []
+    #search haiku-ngrams from data-ngrams
+    for ngram in n_grams:
+        if ngram in corpus_ngrams:
+            result.append(ngram)
+
+    assert result == n_grams
 
 if __name__=="__main__":
-    pass
+    test_markov_chain()
